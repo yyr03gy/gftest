bc08d366211df87441e24efd562b115b5cf41e7b
yyr03gy <yyr03gy@hotmail.com>
2018-02-04 16:25:48 +0800

    1.change install_docker.sh run user to root

--- docker_and_docker-compose_auto_install/install_docker.sh
+++ docker_and_docker-compose_auto_install/install_docker.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
+#check the run user
+[ "`whoami`" ~= "root" ] && {
+    echo "please run this script on root";
+    exit 1
+}
+
 #update the apt package index
-sudo apt-get update
+apt-get update
 
 #Install packages to allow apt to use a repository over HTTPS
-sudo apt-get -y install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
 #Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 #Verify fingerprint
-sudo apt-key fingerprint 0EBFCD88 | grep -q "Docker Release (CE deb) <docker@docker.com>" || {
+apt-key fingerprint 0EBFCD88 | grep -q "Docker Release (CE deb) <docker@docker.com>" || {
         echo "Docker’s official GPG key ERROR ,please check the GPG key";
         exit 1
 }
 
 
 #set up the Docker stable repository
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
    
 #update the apt package index
-sudo apt-get update
+apt-get update
 
 
 #Install the latest version of Docker CE
-sudo apt-get -y install docker-ce
+apt-get -y install docker-ce
    
 #Verify that Docker CE is installed correctly
-sudo docker run hello-world && {
+docker run hello-world && {
         echo -e "\033[1;32minstall docker  lastest success\033[0m";
 } || {
         echo "\033[1;31minstall docker  lastest failed\033[0m";
@@ -43,8 +49,8 @@ sudo docker run hello-world && {
 
 
 #install the lastest docker-compose(1.18)
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o \
-/usr/bin/docker-compose && sudo chmod +x /usr/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o \
+/usr/bin/docker-compose && chmod +x /usr/bin/docker-compose
 
 #check the docker-compose version
 docker-compose --version | grep -q "1.18.0" && {
